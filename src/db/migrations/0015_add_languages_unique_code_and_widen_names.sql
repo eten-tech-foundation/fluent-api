a8f31946a12e7ee9255d1cadc3b4743d95755d4c
AnuMonachan <anumonachan9953@gmail.com>
2026-08-07 11:46:49 +0530

    remove extra migration file

--- src/db/migrations/0015_add_languages_unique_code_and_widen_names.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "languages" ALTER COLUMN "lang_name" SET DATA TYPE varchar(255);--> statement-breakpoint
-ALTER TABLE "languages" ALTER COLUMN "lang_name_localized" SET DATA TYPE varchar(255);--> statement-breakpoint
-ALTER TABLE "languages" ADD CONSTRAINT "languages_lang_code_iso_639_3_unique" UNIQUE("lang_code_iso_639_3");
